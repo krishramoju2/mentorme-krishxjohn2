361d873c44039bae4759b466976af694a949e3b2
krishramoju2 <futureai.incorporated@gmail.com>
2025-08-13 20:20:05 +0530

    Create stats.js

---
+++ src/data/stats.js
@@ -0,0 +1,7 @@
+// This file contains the data for the stats section.
+export const stats = [
+  { count: '600', text: 'Courses on the Platform' },
+  { count: '20', text: 'Mentors from the Industry' },
+  { count: '100', text: 'Students places successfully' },
+  { count: '55', text: 'Average Salary Hike' },
+];
